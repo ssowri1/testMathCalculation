dc682a4d43aa515b224e5324f03a0bc7ac2688eb
SowrirajanS <ssowri1@gmail.com>
2020-06-27 22:45:01 +0530

    Initial Commit

---
+++ Testdgs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testdgs
+//
+//  Created by Sowrirajan S on 27/06/20.
+//  Copyright © 2020 com.GyroscopePOC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
